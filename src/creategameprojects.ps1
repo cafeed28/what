944a78b28f75883ee33711cf68131740e9feeca3
hampta <56561445+hampta@users.noreply.github.com>
2021-07-13 21:33:01 +0300

    Create creategameprojects.ps1

---
+++ src/creategameprojects.ps1
@@ -0,0 +1,42 @@
+if (-NOT ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator"))
+{
+	Write-Warning "You do not have Administrator rights to run this script!`nPlease re-run this script as an Administrator!"
+	pause
+	exit
+}
+
+$ErrorActionPreference= 'silentlycontinue'
+# This is the path to your steamworks dev folder (depo). This should be the folder with hl2.exe inside of it.
+# Your local momentum folder will be sym linked INSIDE this folder as momentum
+$path = "E:\Steamworks\dev\csso"
+
+# Boot arguments
+$hl2exe = Join-Path $path hl2.exe 
+$hl2args = "-game csso -insecure -window -w 1600 -h 900 -novid +developer 2 -console "
+
+$csso_sym = Join-Path $path csso # Writing as (steamworks path)/momentum
+$bin_sym = Join-Path $path bin
+$csso = [System.IO.Path]::GetFullPath("..\game\csso") # This is your local dev folder
+$bin = [System.IO.Path]::GetFullPath("..\game\bin")
+cmd /c mklink /D $csso_sym $csso
+cmd /c mklink /J $bin $bin_sym # Hard link so visual studio can put the file in the proper place automatically
+
+$data = @"
+<?xml version="1.0" encoding="utf-8"?>
+<Project ToolsVersion="12.0" xmlns="http://schemas.microsoft.com/developer/msbuild/2003">
+  <PropertyGroup Condition="'`$(Configuration)|`$(Platform)'=='Debug|Win32'">
+    <LocalDebuggerCommand>$hl2exe</LocalDebuggerCommand>
+    <LocalDebuggerCommandArguments>$hl2args</LocalDebuggerCommandArguments>
+    <DebuggerFlavor>WindowsLocalDebugger</DebuggerFlavor>
+  </PropertyGroup>
+  <PropertyGroup Condition="'`$(Configuration)|`$(Platform)'=='Release|Win32'">
+    <LocalDebuggerCommand>$hl2exe</LocalDebuggerCommand>
+    <LocalDebuggerCommandArguments>$hl2args</LocalDebuggerCommandArguments>
+    <DebuggerFlavor>WindowsLocalDebugger</DebuggerFlavor>
+  </PropertyGroup>
+</Project>
+"@
+
+$data | Out-File game\client\client_csso.vcxproj.user utf8
+
+"`nDebug and launch added to client_csso.vcxproj`n"
