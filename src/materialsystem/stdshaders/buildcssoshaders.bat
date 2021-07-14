8a5c4e33374e0aaebee8fbe2c8d51f0eb8a46bd8
hampta <art.mr.hampta@gmail.com>
2021-07-14 11:31:05 +0300

    compile shaders

--- src/materialsystem/stdshaders/buildcssoshaders.bat
+++ src/materialsystem/stdshaders/buildcssoshaders.bat
@@ -5,10 +5,10 @@ rem ================================
 rem ==== MOD PATH CONFIGURATIONS ===
 
 rem == Set the absolute path to your mod's game directory here ==
-set GAMEDIR=%cd%\..\..\..\..\cs_source\mp\game\csso
+set GAMEDIR=%cd%\..\..\..\game\csso
 
 rem == Set the relative or absolute path to Source SDK Base 2013 Singleplayer\bin ==
-set SDKBINDIR="E:\Games\Steam\steamapps\common\Counter-Strike Source\bin"
+set SDKBINDIR="D:\Games\Counter-Strike Source\bin"
 
 rem ==  Set the Path to your mod's root source code ==
 rem This should already be correct, accepts relative paths only!
