ffefa18ece1e1243a0e430a126b6a9fc231baa52
PiMoNFeeD <zarod2005@yandex.ru>
2021-08-25 16:10:38 +0300

    comment fix

--- src/game/server/cstrike/te_shotgun_shot.cpp
+++ src/game/server/cstrike/te_shotgun_shot.cpp
@@ -61,7 +61,7 @@ IMPLEMENT_SERVERCLASS_ST_NOBASE(CTEFireBullets, DT_TEFireBullets)
 	SendPropVector( SENDINFO(m_vecOrigin), -1, SPROP_COORD ),
 	SendPropAngle( SENDINFO_VECTORELEM( m_vecAngles, 0 ), 13, 0 ),
 	SendPropAngle( SENDINFO_VECTORELEM( m_vecAngles, 1 ), 13, 0 ),
-	SendPropInt( SENDINFO( m_iWeaponID ), 6, SPROP_UNSIGNED ), // max 31 weapons
+	SendPropInt( SENDINFO( m_iWeaponID ), 6, SPROP_UNSIGNED ), // max 63 weapons
 	SendPropInt( SENDINFO( m_iMode ), 1, SPROP_UNSIGNED ),
 	SendPropInt( SENDINFO( m_iSeed ), NUM_BULLET_SEED_BITS, SPROP_UNSIGNED ),
 	SendPropInt( SENDINFO( m_iPlayer ), 6, SPROP_UNSIGNED ), 	// max 64 players, see MAX_PLAYERS
